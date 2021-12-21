12cc013a3ecf89ba90d5a292797f82ab1d25b534
Chris Simpkins <chris@sourcefoundry.org>
2021-12-21 14:42:49 -0500

    add unary neg, subtraction, and scalar mult operator overload benchmarks

--- benches/vector_operator_benchmarks.rs
+++ benches/vector_operator_benchmarks.rs
@@ -13,16 +13,67 @@ const VEC_F64_3D: Vector<f64, 3_usize> = Vector {
 
 // ================================
 //
+// Unary neg
+//
+// ================================
+
+fn vector_unary_neg_i32_3d() {
+    let _ = -VEC_I32_3D;
+}
+
+fn vector_unary_neg_f64_3d() {
+    let _ = -VEC_F64_3D;
+}
+
+// ================================
+//
 // Addition
 //
 // ================================
 
-fn vector_add_i32() {
+fn vector_add_i32_3d() {
     let _ = black_box(VEC_I32_3D + VEC_I32_3D);
 }
 
-fn vector_add_f64() {
+fn vector_add_f64_3d() {
     let _ = black_box(VEC_F64_3D + VEC_F64_3D);
 }
 
-iai::main!(vector_add_i32, vector_add_f64,);
+// ================================
+//
+// Subtraction
+//
+// ================================`
+
+fn vector_subtract_i32_3d() {
+    let _ = black_box(VEC_I32_3D - VEC_I32_3D);
+}
+
+fn vector_subtract_f64_3d() {
+    let _ = black_box(VEC_F64_3D - VEC_F64_3D);
+}
+
+// ================================
+//
+// Scalar multiplication
+//
+// ================================`
+
+fn vector_multiplication_i32_3d() {
+    let _ = black_box(VEC_I32_3D * 10);
+}
+
+fn vector_multiplication_f64_3d() {
+    let _ = black_box(VEC_F64_3D * 10.0);
+}
+
+iai::main!(
+    vector_unary_neg_i32_3d,
+    vector_unary_neg_f64_3d,
+    vector_add_i32_3d,
+    vector_add_f64_3d,
+    vector_subtract_i32_3d,
+    vector_subtract_f64_3d,
+    vector_multiplication_i32_3d,
+    vector_multiplication_f64_3d,
+);
