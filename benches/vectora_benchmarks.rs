ce2b013a7f4be985e3709c60d8dc2b3dae0de8f4
Chris Simpkins <chris@sourcefoundry.org>
2021-12-20 22:21:06 -0500

    initial commit

---
+++ benches/vectora_benchmarks.rs
@@ -0,0 +1,28 @@
+use iai::{black_box, main};
+
+use vectora::Vector;
+
+// ================================
+//
+// Vector Initialization
+//
+// ================================
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Zero vectors
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+fn vector_initialization_i32_zero() -> Vector<i32, 3> {
+    Vector::<i32, 3>::zero()
+}
+
+fn vector_initialization_f64_zero() -> Vector<f64, 3> {
+    Vector::<f64, 3>::zero()
+}
+
+
+
+iai::main!(
+    vector_initialization_i32_zero,
+    vector_initialization_f64_zero,
+);
