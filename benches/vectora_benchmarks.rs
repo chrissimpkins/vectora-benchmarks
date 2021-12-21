e75b5d1f859f46f4b90b3f8c857823ecdddacd62
Chris Simpkins <chris@sourcefoundry.org>
2021-12-21 08:31:57 -0500

    add i32 3d Vector init benchmarks

--- benches/vectora_benchmarks.rs
+++ benches/vectora_benchmarks.rs
@@ -21,9 +21,54 @@ fn vector_initialization_f64_zero() -> Vector<f64, 3> {
     Vector::<f64, 3>::zero()
 }
 
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~
+// i32 3D Vectors
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+fn vector_initialization_i32_3d_array() {
+    let _ = Vector::<i32, 3>::from(black_box([1, 2, 3]));
+}
+
+fn vector_initialization_i32_3d_iter_over_array() {
+    let _ = black_box([1, 2, 3]).into_iter().collect::<Vector<i32, 3>>();
+}
+
+fn vector_initialization_i32_iter_over_vec() {
+    let _ = black_box(vec![1, 2, 3])
+        .into_iter()
+        .collect::<Vector<i32, 3>>();
+}
+
+fn vector_initialization_i32_try_from_array_slice() {
+    let arr = [1, 2, 3];
+    let sli = &arr[..];
+    let _ = Vector::<i32, 3>::try_from(black_box(sli)).unwrap();
+}
+
+fn vector_initialization_i32_try_from_vec_slice() {
+    let slvec = vec![1, 2, 3];
+    let sli = &slvec[..];
+    let _ = Vector::<i32, 3>::try_from(black_box(black_box(sli))).unwrap();
+}
+
+fn vector_initialization_i32_try_from_vec_reference() {
+    let vect = vec![1, 2, 3];
+    let _ = Vector::<i32, 3>::try_from(&vect).unwrap();
+}
+
+fn vector_initialization_i32_try_from_vec() {
+    let vect = vec![1, 2, 3];
+    let _: Vector<i32, 3> = Vector::try_from(black_box(vect)).unwrap();
+}
 
 iai::main!(
     vector_initialization_i32_zero,
     vector_initialization_f64_zero,
+    vector_initialization_i32_3d_array,
+    vector_initialization_i32_3d_iter_over_array,
+    vector_initialization_i32_iter_over_vec,
+    vector_initialization_i32_try_from_array_slice,
+    vector_initialization_i32_try_from_vec_slice,
+    vector_initialization_i32_try_from_vec_reference,
+    vector_initialization_i32_try_from_vec,
 );
